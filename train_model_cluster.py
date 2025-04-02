9bf292bff77a942847f350afb74469b5df63d1b9
bruhchup <haydenbanks04@gmail.com>
2025-04-01 23:45:34 -0400

    updating branch

--- train_model_cluster.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import KMeans
-
-df = pd.read_excel('Sentiment-Analysis-WLB/data_cleaning/data/spread_sheet_for_DIB.xlsx', header=[0,1])
-
-print(df)
